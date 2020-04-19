b129a3aed8ce9897b05b32d180ea92a4226c52ef
Michael Rieberer <mrieberer@gmail.com>
2020-04-19 13:07:46 +0200

    Implemented dynamic test + 100% test coverage

--- src/test/java/com/michaelR/IntroProgramming/Assignment_2/AdvancedCalculatorTest.java
+++ src/test/java/com/michaelR/IntroProgramming/Assignment_2/AdvancedCalculatorTest.java
@@ -1,38 +1,87 @@
 package com.michaelR.IntroProgramming.Assignment_2;
-
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 import static org.junit.Assert.*;
-
+import java.util.Random;
+import java.util.logging.Logger;
 import org.junit.Test;
-
 import com.calculator.classes.AdvancedCalculator;
+import com.calculator.classes.BasicCalculator;
 
 public class AdvancedCalculatorTest {
 	
 	AdvancedCalculator ac = new AdvancedCalculator();
+	Random random = new Random();
+	public static final Logger LOG = Logger.getLogger(BasicCalculator.class.getName());
+	int NumberOfRepetitions = 1;
 
 	@Test
 	public void testCubeRoot() {
-		assertEquals(ac.cubeRoot(27), 3, 0.001);
+		double firstTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			expectedResult = pow(firstTestParameter, 0.333333);
+			LOG.info("Testing method AdvancedCalculator.cubeRoot with parameter: " + firstTestParameter);
+			assertEquals(ac.cubeRoot(firstTestParameter), expectedResult, 0.001);
+		}
 	}
 	
 	@Test
 	public void testRemainder() {
-		assertEquals(ac.remainder(5, 6), 5, 0.001);
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter % secondTestParameter;
+			LOG.info("Testing method AdvancedCalculator.remainder with parameters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(ac.remainder(firstTestParameter, secondTestParameter), expectedResult, 0.001);
+		}
 	}
 	
 	@Test
 	public void testSquare() {
-		assertEquals(ac.square(11), 121, 0.001);
+		double firstTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter * firstTestParameter;
+			LOG.info("Testing method AdvancedCalculator.square with parameter: " + firstTestParameter);
+			assertEquals(ac.square(firstTestParameter), expectedResult, 0.001);
+		}
 	}
 	
 	@Test
 	public void testSquareRoot() {
-		assertEquals(ac.squareRoot(121), 11, 0.001);
+		double firstTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			expectedResult = sqrt(firstTestParameter);
+			LOG.info("Testing method AdvancedCalculator.squareRoot with parameter: " + firstTestParameter);
+			assertEquals(ac.squareRoot(firstTestParameter), expectedResult, 0.001);
+		}
 	}
 	
 	@Test
 	public void testToThePowerOf() {
-		assertEquals(ac.toThePowerOf(2, 5), 32, 0.001);
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = pow(firstTestParameter, secondTestParameter);
+			LOG.info("Testing method AdvancedCalculator.toThePowerOf with parameters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(ac.toThePowerOf(firstTestParameter, secondTestParameter), expectedResult, 0.001);
+		}
 	}
 	
 }
--- src/test/java/com/michaelR/IntroProgramming/Assignment_2/BasicCalculatorTest.java
+++ src/test/java/com/michaelR/IntroProgramming/Assignment_2/BasicCalculatorTest.java
@@ -1,34 +1,75 @@
 package com.michaelR.IntroProgramming.Assignment_2;
 
 import static org.junit.Assert.*;
-
+import java.util.Random;
+import java.util.logging.Logger;
 import org.junit.Test;
-
 import com.calculator.classes.BasicCalculator;
 
 public class BasicCalculatorTest {
 	
 	BasicCalculator bc = new BasicCalculator();
+	Random random = new Random();
+	public static final Logger LOG = Logger.getLogger(BasicCalculator.class.getName());
+	int NumberOfRepetitions = 1;
 
 	@Test
 	public void testAddition() {
-		assertEquals(bc.addition(3, 5), 8, 0);
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter + secondTestParameter;
+			LOG.info("Testing method BasicCalculator.addition with parmaeters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(bc.addition(firstTestParameter, secondTestParameter), expectedResult, 0);
+		}
 	}
 	
 	@Test
 	public void testSubtraction() {
-		assertEquals(bc.subtraction(9, 5), 4, 0);
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter - secondTestParameter;
+			LOG.info("Testing method BasicCalculator.subtraction with parmaeters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(bc.subtraction(firstTestParameter, secondTestParameter), expectedResult, 0);
+		}
 	}
-	
 	@Test
 	public void testMultiplication() {
-		assertEquals(bc.multiplication(3, 5), 15, 0);
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter * secondTestParameter;
+			LOG.info("Testing method BasicCalculator.multiplication with parmaeters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(bc.multiplication(firstTestParameter, secondTestParameter), expectedResult, 0);
+		}
 	}
-	
 	@Test
-	public void testDivsion() {
-		assertEquals(bc.division(30, 5), 6, 0);
-	} 
+	public void testDivision() {
+		double firstTestParameter = 0.0;
+		double secondTestParameter = 0.0;
+		double expectedResult = 0.0;
+		
+		for (int i = 0; i < NumberOfRepetitions; i++) {
+			firstTestParameter = random.nextDouble();
+			secondTestParameter = random.nextDouble();
+			expectedResult = firstTestParameter / secondTestParameter;
+			LOG.info("Testing method BasicCalculator.division with parmaeters: " + firstTestParameter + " and " + secondTestParameter);
+			assertEquals(bc.division(firstTestParameter, secondTestParameter), expectedResult, 0);
+		}
+	}
 
 }
 
